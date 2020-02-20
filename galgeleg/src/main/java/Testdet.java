2c61e33cea60e5157990710de52781b058db4570
antonoeschmidt <s163053@student.dtu.dk>
2020-02-20 12:18:51 +0100

    Brugergodkendelse via RMI

---
+++ galgeleg/src/main/java/Testdet.java
@@ -0,0 +1,15 @@
+import kong.unirest.Unirest;
+
+public class Testdet {
+
+    public static void main(String[] args) {
+        searchCompany("egn");
+
+    }
+
+    public static void searchCompany(String firma) {
+        System.out.println(Unirest.get("https://cvrapi.dk/api?search=" + firma + "&country=dk&format=xml")
+                .queryString("vat", "")
+                .asString().getBody());
+    }
+}
